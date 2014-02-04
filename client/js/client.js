7187cfa2014925a379620777ac54b3303edea8ea
Ng Guoyou <fauxface+github@gmail.com>
2014-02-04 22:12:35 +0800

    Refactor client-side code.

--- client/js/client.js
+++ client/js/client.js
@@ -1,154 +1,53 @@
 (function () {
   'use strict';
 
-  var name      = false;
-  var content   = document.getElementById('content');
-  var input     = document.getElementById('input');
-  var inputForm = document.getElementById('input-form');
-  var status    = document.getElementById('status');
-  var notify    = document.getElementById('notify');
   var serverAddress = 'ws://hidoi.moebros.org:8084';
-  input.value = ''; // Clear on init
-  var currentBoxActors = 0;
-  var currentBoxes = 0;
-  var previousAuthor = null;
+
+  var ui = new UI({
+    content:       document.getElementById('content'),
+    input:         document.getElementById('input'),
+    inputForm:     document.getElementById('input-form'),
+    status:        document.getElementById('status'),
+    notifyEnabled: document.getElementById('notify')
+  });
 
   if (typeof (window.WebSocket || window.MozWebSocket) === 'undefined') {
-    content.innerHTML = 'Websocket support required.';
+    document.getElementByTag('body').innerHTML = 'Websocket support required.';
     return;
   }
 
-  var notification = (window.Notification || window.webkitNotifications);
-  if (typeof notification !== 'undefined' && notification.permission !== 'denied') {
-    notification.requestPermission();
-  }
-
   var connection = new WebSocket(serverAddress);
+  ui.setConnection(connection);
 
   connection.onopen = function () {
-    console.log('Connection established with ' + serverAddress);
-    status.innerHTML = 'Connected.';
+    console.log('Connection to ' + serverAddress + ' established');
+    ui.setStatus('Connected.');
   };
 
   connection.onerror = function (e) {
-    console.log('Error: ' + e);
-    status.innerHTML = 'Disconnected.';
+    console.log('Connection error', e);
+    ui.setStatus('Disconnected.');
   };
 
   connection.onmessage = function (message) {
-    // TODO JSON validation with try/catch
-    var json = JSON.parse(message.data);
+    try {
+      var obj = JSON.parse(message.data);
+    } catch (e if e instanceof SyntaxError) {
+      console.log('Invalid message', e);
+      return;
+    }
 
-    if (json.type === 'history') {
-      for (var i = 0; i < json.data.length; i++) {
-        addLine(JSON.parse(json.data[i]).data);
-      }
-    } else if (json.type === 'message') {
-      addLine(json.data);
-
-      if (notification.permission === 'granted' && notify.checked === true) {
-        var notice = new Notification('comicchat', { body: json.data.author + ": " + json.data.text });
-      }
-    } else {
-      console.log(json);
+    switch (obj.type) {
+    case 'history':
+      ui.addHistory(obj.data);
+      break;
+    case 'message':
+      ui.addLine(obj.data);
+      ui.notify(obj.data);
+      break;
+    default:
+      console.log('Unknown message', obj);
+      break;
     }
   };
-
-  // User Interface
-
-  inputForm.onsubmit = function (e) {
-    connection.send(input.value);
-    input.placeholder = 'Chat...';
-    inputForm.reset();
-    e.preventDefault();
-  };
-
-  function addLine (message) {
-    var maxActorsPerBox = 2;
-
-    // Make a new box if
-    // * We hit maximum number of actors in a box
-    // * No boxes
-    // * It's a monologue
-    var newBox =
-      currentBoxActors >= maxActorsPerBox ||
-      currentBoxes === 0 ||
-      previousAuthor === message.author;
-
-    if (newBox === true) {
-      content.appendChild(makeBox());
-      window.scrollTo(0, document.body.scrollHeight);
-      currentBoxActors = 0;
-      currentBoxes++;
-    }
-
-    var boxes = content.querySelectorAll(".box");
-    var box = boxes[boxes.length - 1];
-    var flip = currentBoxActors >= maxActorsPerBox / 2
-    box.appendChild(makeActor(message, flip));
-    currentBoxActors++;
-    previousAuthor = message.author;
-  };
-
-  function makeBox () {
-    var boxTemplate = document.getElementById('box-template').innerHTML;
-    var box = document.createElement('div');
-    box.innerHTML = boxTemplate;
-
-    return box.getElementsByTagName('div')[0];
-  }
-
-  function makeActor (message, flip) {
-    var actorTemplate = document.getElementById('actor-template').innerHTML;
-    var characters = [
-      {
-        name: 'hugh',
-        images: ['angry', 'bored', 'coy', 'happy', 'laugh', 'neutral', 'sad', 'scared', 'shout']
-      }, {
-        name: 'lance',
-        images: ['0']
-      }, {
-        name: 'jordan',
-        images: ['angry', 'bored', 'coy', 'happy', 'laugh', 'neutral', 'sad', 'scared', 'shout']
-      }, {
-        name: 'kwensa',
-        images: ['angry', 'bored', 'coy', 'happy', 'laugh', 'neutral', 'sad', 'scared', 'shout']
-      }
-    ];
-
-    var character = characters[getHashCode(message.author) % characters.length];
-    var avatar = document.createElement('img');
-    var avatarImageIndex = getHashCode(message.text + ' ' + message.author + ' ' + currentBoxes) % character.images.length;
-    avatar.src = './res/avatars/' + character.name + '/' + character.images[avatarImageIndex] + '.png';
-
-    // Make characters face each other
-    if (flip === true) {
-      if (avatar.classList) {
-        avatar.classList.add('flip-horizontal');
-      } else {
-        avatar.className += ' flip-horizontal';
-      }
-    }
-
-    var actor = document.createElement('div');
-    actor.innerHTML = actorTemplate;
-    actor.querySelector('.text').appendChild(document.createTextNode(message.text));
-    actor.querySelector('.name').appendChild(document.createTextNode(message.author));
-    actor.querySelector('.avatar').appendChild(avatar);
-
-    return actor.getElementsByTagName('div')[0];
-  }
-
-  // Utility
-
-  function getHashCode (string) {
-    var hash = 0;
-    if (string.length == 0) return hash;
-    for (var i = 0; i < string.length; i++) {
-      var char = string.charCodeAt(i);
-      hash = ((hash << 5) - hash) + char;
-      hash = hash & hash; // Convert to 32bit integer
-    }
-    return Math.abs(hash);
-  }
 })();
---
+++ client/js/ui.js
@@ -0,0 +1,151 @@
+function UI (elements) {
+  this.content       = elements.content;
+  this.input         = elements.input;
+  this.inputForm     = elements.inputForm;
+  this.status        = elements.status;
+  this.notifyEnabled = elements.notifyEnabled;
+
+  this.connection   = null;
+  this.notification = null;
+
+  this.maxActorsPerBox = 2;
+  this.currentBoxActors = 0;
+  this.currentBoxes = 0;
+  this.previousAuthor = null;
+
+  this.input.value = ''; // Clear on init
+
+  this.setupShortcuts();
+  this.setupNotifications();
+  this.loadCharacterManifest();
+}
+
+UI.prototype = {
+  setConnection: function (connection) {
+    this.connection = connection;
+  },
+
+  setStatus: function (status) {
+    this.status.innerHTML = status;
+  },
+
+  setupNotifications: function () {
+    this.notification = (window.Notification || window.webkitNotifications);
+    if (typeof this.notification !== 'undefined' && this.notification.permission !== 'denied') {
+      this.notification.requestPermission();
+    }
+  },
+
+  notify: function (data) {
+    if (this.notification.permission === 'granted' && this.notifyEnabled.checked === true) {
+      new Notification('comicchat', {
+        lang: 'en-US',
+        icon: './res/icon.gif',
+        body: data.author + ": " + data.text
+      });
+    }
+  },
+
+  setupShortcuts: function () {
+    this.inputForm.onsubmit = function (e) {
+      e.preventDefault();
+      this.connection.send(input.value);
+      this.input.placeholder = 'Chat...';
+      this.inputForm.reset();
+    }.bind(this);
+  },
+
+  addHistory: function (history) {
+    for (var i = 0; i < history.length; i++) {
+      this.addLine(JSON.parse(history[i]).data);
+    }
+  },
+
+  addLine: function (message) {
+    // Make a new box if
+    // * We hit maximum number of actors in a box
+    // * No boxes
+    // * It's a monologue
+    var newBox =
+      this.currentBoxActors >= this.maxActorsPerBox ||
+      this.currentBoxes === 0 ||
+      this.previousAuthor === message.author;
+
+    if (newBox === true) {
+      this.content.appendChild(this.makeBox());
+      window.scrollTo(0, document.body.scrollHeight);
+      this.currentBoxActors = 0;
+      this.currentBoxes++;
+    }
+
+    var boxes = this.content.querySelectorAll(".box");
+    var box = boxes[boxes.length - 1];
+    var flip = this.currentBoxActors >= this.maxActorsPerBox / 2;
+
+    box.appendChild(this.makeActor(message, flip));
+    this.currentBoxActors++;
+    this.previousAuthor = message.author;
+  },
+
+  makeBox: function () {
+    var boxTemplate = document.getElementById('box-template').innerHTML;
+    var box = document.createElement('div');
+    box.innerHTML = boxTemplate;
+
+    return box.getElementsByTagName('div')[0];
+  },
+
+  makeActor: function (message, flip) {
+    var actorTemplate = document.getElementById('actor-template').innerHTML;
+    var characters = this.characters;
+
+    var character = characters[this.getHashCode(message.author) % characters.length];
+    var avatar = document.createElement('img');
+    var avatarImageIndex = this.getHashCode(message.text + ' ' + message.author + ' ' + this.currentBoxes) % character.images.length;
+    avatar.src = './res/avatars/' + character.name + '/' + character.images[avatarImageIndex] + '.png';
+
+    // Make characters face each other
+    if (flip === true) {
+      if (avatar.classList) {
+        avatar.classList.add('flip-horizontal');
+      } else {
+        avatar.className += ' flip-horizontal';
+      }
+    }
+
+    var actor = document.createElement('div');
+    actor.innerHTML = actorTemplate;
+    actor.querySelector('.text').appendChild(document.createTextNode(message.text));
+    actor.querySelector('.name').appendChild(document.createTextNode(message.author));
+    actor.querySelector('.avatar').appendChild(avatar);
+
+    return actor.getElementsByTagName('div')[0];
+  },
+
+  loadCharacterManifest: function () {
+    var request = new XMLHttpRequest;
+    request.open('GET', './res/avatars/manifest.json', true);
+    request.send();
+
+    var that = this;
+    request.onload = function() {
+      that.characters = JSON.parse(this.response);
+    };
+  },
+
+  getHashCode: function (string) {
+    var hash = 0;
+
+    if (string.length == 0) {
+      return hash;
+    }
+
+    for (var i = 0; i < string.length; i++) {
+      var char = string.charCodeAt(i);
+      hash = ((hash << 5) - hash) + char;
+      hash = hash & hash; // Convert to 32bit integer
+    }
+
+    return Math.abs(hash);
+  }
+};
