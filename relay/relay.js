bcb81f044cd924080ca4fbe1cbbb3cb989287779
Ng Guoyou <fauxface+github@gmail.com>
2014-02-06 19:23:03 +0800

    Rewrite IRC relay.

--- relay/relay.js
+++ relay/relay.js
@@ -1,146 +1,188 @@
-// WARNING! BAD CODE AHEAD!
+/* jslint node: true */
+
+'use strict';
+
+function log (text) {
+  console.log("\n" + (new Date()) + "\n" + text);
+}
 
 var net = require('net');
 var tls = require('tls');
 var WebSocketClient = require('websocket').client;
-var irc = {};
+
 var config = {
-  user: {
+  cchat: {
+    nick: 'ircrelay',
+    room: '#!',
+    host: 'hidoi.moebros.org',
+    port: 8084
+  },
+  irc: {
     nick: 'comicrelay',
     user: 'comic',
     real: 'relay',
-    pass: ''
-  },
-  server: {
-    addr: '',
+    channel: '#',
+    host: '',
     port: 6697,
     ssl: true
-  },
-  chans: ['#'],
-};
-
-var wsConfig = {
-  server: {
-    addr: 'hidoi.moebros.org',
-    port: 8084
-  },
-  cchat: {
-    room: '#!'
   }
 };
 
-// WEBSOCKET CONNECTION
-var ws = new WebSocketClient();
+
+
+// COMIC CHAT CONNECTION
+
 var wsConnection;
-ws.on('connect', function (connection) {
-  console.log('Websocket client connected');
-  wsConnection = connection;
-  connection.sendUTF(JSON.stringify({
-    type: 'join',
-    room: wsConfig.cchat.room
-  }));
+var wsRetryHandlerID = null;
 
-  // Register
-  connection.sendUTF(JSON.stringify({
-    type: 'message',
-    room: wsConfig.cchat.room,
-    text: 'ircrelay'
-  }));
+function makeComicChat () {
+  var reconnectFunction = function () {
+    if (wsRetryHandlerID === null) {
+      wsRetryHandlerID = setInterval(function () {
+        log('CC: Reconnecting...');
+        makeComicChat();
+      }, 10000);
+    }
+  };
 
-  connection.sendUTF(JSON.stringify({
-    type: 'message',
-    room: wsConfig.cchat.room,
-    text: 'IRC relay to ' + config.server.addr + ' ' +  config.chans[0] + ' connected.'
-  }));
-});
-ws.connect('ws://' + wsConfig.server.addr + ':' + wsConfig.server.port);
+  function addHandlers (ws) {
+    ws.on('connect', function (connection) {
+      log('CC: Websocket client connected to comic chat.');
+      wsConnection = connection;
+      if (wsRetryHandlerID !== null) {
+        clearInterval(wsRetryHandlerID);
+        wsRetryHandlerID = null;
+      }
+
+      // Join room, register nick, announce to room that relay has joined.
+      [
+        { type: 'join',    room: config.cchat.room },
+        { type: 'message', room: config.cchat.room, text: config.cchat.nick },
+        { type: 'message', room: config.cchat.room, text: 'IRC relay to ' + config.irc.host + ' connected.' }
+      ].forEach(function (message) {
+        connection.sendUTF(JSON.stringify(message));
+      });
+
+      connection.on('error', function (e) {
+        log('CC: Connection error', e);
+        reconnectFunction();
+      });
+
+      connection.on('close', function (e) {
+        log('CC: Connection closed', e);
+        reconnectFunction();
+      });
+    });
+
+    return ws;
+  }
+
+  var ws = addHandlers(new WebSocketClient());
+  ws.on('connectFailed', function (e) {
+    log('CC: Conenction failed', e);
+    reconnectFunction();
+  });
+  ws.connect('ws://' + config.cchat.host + ':' + config.cchat.port);
+}
+
+makeComicChat();
+
+
 
 // IRC CONNECTION
-// Self-signed certs
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 
-if (config.server.ssl === true) {
-  irc.socket = tls.connect(config.server.port, config.server.addr);
-} else {
-  irc.socket = new net.Socket();
-}
-
-irc.socket.on('data', function (data) {
-  data = data.split('\n');
-  for (var i = 0; i < data.length; i++) {
-    console.log('RECV -', data[i]);
-    if (data !== '') {
-      irc.handle(data[i].slice(0, -1));
-    }
-  }
-});
-
-var connectFunction =  function () {
-  console.log('Established connection, registering...');
-  irc.on(/^PING :(.+)$/i, function(info) {
-    irc.raw('PONG :' + info[1]);
-  });
-
-  setTimeout(function () {
-    irc.raw('NICK ' + config.user.nick);
-    irc.raw('USER ' + config.user.user + ' 8 * :' + config.user.real);
-
-    setTimeout(function () {
-      for (var i = 0; i < config.chans.length; i++) {
-        irc.raw('JOIN ' + config.chans[i]);
-      }
-    }, 4000);
-  }, 1000);
-};
-
-if (config.server.ssl === true) {
-  irc.socket.on('secureConnect', connectFunction);
-} else {
-  irc.socket.on('connect', connectFunction);
-}
-
-irc.socket.setEncoding('utf-8');
-irc.socket.setNoDelay();
-
-if (config.server.ssl === false) {
-  irc.socket.connect(config.server.port, config.server.addr);
-}
-
-irc.handle = function (data) {
-  var i, info;
-  for (i = 0; i < irc.listeners.length; i++) {
-    info = irc.listeners[i][0].exec(data);
-    if (info) {
-      irc.listeners[i][1](info, data);
-      if (irc.listeners[i][2]) {
-        irc.listeners.splice(i, 1);
-      }
-    }
-  }
-
-  var tokens = data.split(' ');
-  if (tokens[1] === 'PRIVMSG') {
-    wsConnection.send(JSON.stringify({
-      type: 'message',
-      text: data.split(' :')[1],
-      room: wsConfig.cchat.room,
-      author: tokens[0].split('!')[0].substring(1),
-      spoof: true
-    }));
-  }
-};
-
+var irc = {};
 irc.listeners = [];
-irc.on = function (data, callback) {
-  irc.listeners.push([data, callback, false]);
-};
 
-irc.on_once = function (data, callback) {
-  irc.listeners.push([data, callback, true]);
-};
+function makeIRC() {
+  var connectHandler = function () {
+    log('IRC: established connection, registering...');
 
-irc.raw = function(data) {
-  irc.socket.write(data + '\n', 'ascii', function () {
-    console.log('SENT -', data);
+    irc.on(/^PING :(.+)$/i, function (info) {
+      irc.raw('PONG :' + info[1]);
+    });
+
+    irc.on(/^.+ 001 .+$/i, function () {
+      irc.raw('JOIN ' + config.irc.channel);
+    });
+
+    irc.on(/^:.+!(.+)@.+ PRIVMSG #.+ :(.+)$/i, function (info) {
+      if (wsConnection && wsConnection.send) {
+        log('CC -> RELAY ' + info[1] + ': ' + info[2]);
+        wsConnection.send(JSON.stringify({
+          type: 'message',
+          room: config.cchat.room,
+          text: info[2],
+          author: info[1],
+          spoof: true
+        }));
+      } else {
+        log('IRC->CC Problem with CC connection, not relaying');
+      }
+    });
+
+    irc.raw('NICK ' + config.irc.nick);
+    irc.raw('USER ' + config.irc.user + ' 8 * :' + config.irc.real);
+  };
+
+  if (config.irc.ssl === true) {
+    process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0'; // Self-signed certificates
+    irc.socket = tls.connect(config.irc.port, config.irc.host);
+    irc.socket.on('secureConnect', connectHandler);
+  } else {
+    irc.socket = new net.Socket();
+    irc.socket.on('connect', connectHandler);
+    irc.socket.connect(config.irc.port, config.irc.host);
+  }
+
+  irc.socket.setEncoding('utf-8');
+  irc.socket.setNoDelay();
+
+  irc.handle = function (data) {
+    var info;
+
+    for (var i = 0; i < irc.listeners.length; i++) {
+      info = irc.listeners[i][0].exec(data);
+
+      if (info) {
+        irc.listeners[i][1](info, data);
+
+        if (irc.listeners[i][2]) {
+          irc.listeners.splice(i, 1);
+        }
+      }
+    }
+  };
+
+  irc.on = function (data, callback) {
+    irc.listeners.push([data, callback, false]);
+  };
+
+  irc.on_once = function (data, callback) {
+    irc.listeners.push([data, callback, true]);
+  };
+
+  irc.raw = function(data) {
+    if (data !== '') {
+      irc.socket.write(data + '\n', 'utf-8');
+      log('IRC -> ' + data);
+    }
+  };
+
+  irc.socket.on('data', function (data) {
+    data = data.split("\n");
+
+    for (var i = 0; i < data.length; i++) {
+      if (data[i] !== '') {
+        log('IRC <- ' + data[i]);
+        irc.handle(data[i].slice(0, -1));
+      }
+    }
   });
-};
+
+  irc.socket.on('close', function () {
+    makeIRC();
+  });
+}
+
+makeIRC();
