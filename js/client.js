75ac64bbddfdb11e0a4bd5d1143c1dd7e20c3e0d
Ng Guoyou <fauxface+github@gmail.com>
2016-02-25 00:34:13 +0800

    Updates

---
+++ js/client.js
@@ -0,0 +1,99 @@
+(function () {
+  'use strict';
+
+  var serverAddress = 'ws://localhost:8084';
+
+  var ui = new UI({
+    content:          document.getElementById('content'),
+    input:            document.getElementById('input'),
+    inputForm:        document.getElementById('input-form'),
+    status:           document.getElementById('status'),
+    notifyEnabled:    document.getElementById('notify'),
+    ttsEnabled:       document.getElementById('tts'),
+    roomSwitcher:     document.getElementById('room-switcher'),
+    roomSwitcherForm: document.getElementById('room-switcher-form')
+  });
+
+  if (typeof (window.WebSocket || window.MozWebSocket) === 'undefined') {
+    document.getElementByTag('body').innerHTML = 'Websocket support required.';
+    return;
+  }
+
+  function makeConnection (ws) {
+    ws.onopen = function () {
+      console.log('Connection to ' + serverAddress + ' established');
+      ui.connected();
+
+      if (retryHandlerID !== null) {
+        clearInterval(retryHandlerID);
+        retryHandlerID = null;
+      }
+
+      // Join default room
+      if (window.location.hash === '') {
+        window.location.hash = '#!';
+      }
+
+      ws.send(JSON.stringify({
+        type: 'join',
+        room: window.location.hash
+      }));
+
+      ws.send(JSON.stringify({
+        type: 'history',
+        room: window.location.hash
+      }));
+    };
+
+    ws.onerror = function (e) {
+      console.log('Connection error', e);
+      ui.disconnected();
+    };
+
+    ws.onclose = function (e) {
+      console.log('Connection closed', e);
+      ui.disconnected();
+
+      // Reconnect
+      if (retryHandlerID === null) {
+        retryHandlerID = setInterval(function () {
+          console.log('Attempting reconnect...');
+          ui.reconnecting();
+          var connection = makeConnection(new WebSocket(serverAddress));
+          ui.setConnection(connection);
+        }, 10000);
+      }
+    };
+
+    ws.onmessage = function (message) {
+      var obj;
+
+      try {
+        obj = JSON.parse(message.data);
+      } catch (e) {
+        console.log('Invalid message', e);
+        return;
+      }
+
+      switch (obj.type) {
+      case 'history':
+        ui.addHistory(obj.history);
+        break;
+      case 'message':
+        ui.addLine(obj);
+        ui.tts(obj);
+        ui.notify(obj);
+        break;
+      default:
+        console.log('Unknown message', obj);
+        break;
+      }
+    };
+
+    return ws;
+  }
+
+  var retryHandlerID = null;
+  var connection = makeConnection(new WebSocket(serverAddress));
+  ui.setConnection(connection);
+})();
---
+++ js/ui.js
@@ -0,0 +1,270 @@
+function UI (elements) {
+  this.content          = elements.content;
+  this.input            = elements.input;
+  this.inputForm        = elements.inputForm;
+  this.status           = elements.status;
+  this.notifyEnabled    = elements.notifyEnabled;
+  this.ttsEnabled       = elements.ttsEnabled;
+  this.roomSwitcher     = elements.roomSwitcher;
+  this.roomSwitcherForm = elements.roomSwitcherForm;
+
+  this.connection   = null;
+  this.notification = null;
+
+  this.maxActorsPerBox = 2;
+  this.currentBoxActors = 0;
+  this.currentBoxes = 0;
+  this.currentBox = null;
+  this.previousAuthor = null;
+
+  this.input.value = ''; // Clear on init
+
+  this.synth = window.speechSynthesis;
+  if (typeof this.synth === 'undefined') {
+    this.ttsEnabled.disabled = true;
+  } else {
+    this.synth.speak(new SpeechSynthesisUtterance('')); // Initialize voices
+  }
+
+  this.setupShortcuts();
+  this.setupNotifications();
+  this.loadCharacterManifest();
+}
+
+UI.prototype = {
+  setConnection: function (connection) {
+    this.connection = connection;
+  },
+
+  setStatus: function (status) {
+    this.status.innerHTML = status;
+  },
+
+  connected: function () {
+    this.roomSwitcher.placeholder = window.location.hash;
+    this.roomSwitcher.value = window.location.hash;
+    this.input.placeholder = 'Your nickname...';
+    this.input.disabled = false;
+    this.roomSwitcher.disabled = false;
+    this.setStatus('Connected.');
+  },
+
+  disconnected: function () {
+    this.input.disabled = true;
+    this.input.placeholder = 'No connection';
+    this.roomSwitcher.disabled = true;
+    this.setStatus('Disconnected.');
+  },
+
+  reconnecting: function () {
+    this.setStatus('Reconnecting...');
+  },
+
+  setupNotifications: function () {
+    this.notifyEnabled.onclick = this.requestNotificationsPermission.bind(this);
+    this.notification = this.notification || (window.Notification || window.webkitNotifications);
+    if (typeof this.notification === 'undefined') this.notifyEnabled.disabled = true;
+  },
+
+  requestNotificationsPermission: function () {
+    if (typeof this.notification !== 'undefined' && this.notification.permission === 'default') {
+      this.notification.requestPermission();
+    }
+  },
+
+  notify: function (data) {
+    if (typeof this.notification !== 'undefined' &&
+        this.notification.permission === 'granted' &&
+        this.notifyEnabled.checked === true) {
+      new Notification('comicchat ' + data.room, {
+        lang: 'en-US',
+        icon: './res/icon.gif',
+        body: data.author + ": " + data.text
+      });
+    }
+  },
+
+  getVoicesFor: function (voices, lang) {
+    var result = [];
+    for (var i = 0; i < voices.length; i++) {
+      if (voices[i].lang === lang) {
+        result.push(voices[i]);
+      }
+    }
+
+    return result;
+  },
+
+  tts: function (data) {
+    if (this.ttsEnabled.checked === true && typeof this.synth !== 'undefined') {
+      var utterable = new SpeechSynthesisUtterance(data.text);
+      var voices = this.synth.getVoices();
+      var hashCode = this.getHashCode(data.author);
+
+      var languageVoices = {
+        'ja-JP': this.getVoicesFor(voices, 'ja-JP'),
+        'ko-KR': this.getVoicesFor(voices, 'ko-KR'),
+        'zh-CN': this.getVoicesFor(voices, 'zh-CN')
+      };
+
+      if (data.text.match(/[\u3040-\u309F\u30A0-\u30FF]/g)) {
+        utterable.lang = 'ja-JP';
+      } else if (data.text.match(/[\u1100-\u11FF\u3130-\u318F\uAC00-\uD7AF]/g)) {
+        utterable.lang = 'ko-KR';
+      } else if (data.text.match(/[\u4E00-\u9FFF]/g)) {
+        utterable.lang = 'zh-CN';
+      }
+
+      if (utterable.lang !== '') {
+        var langVoices = languageVoices[utterable.lang];
+        utterable.voice = langVoices[Math.floor(hashCode) % langVoices.length] || null;
+      }
+
+      // Assign random (hashed) voice if not a special language or no voice available for that language
+      if (utterable.voice === '' || utterable.voice === null) {
+        utterable.voice = voices[Math.floor(hashCode) % voices.length];
+      }
+
+      this.synth.speak(utterable);
+    }
+  },
+
+  setupShortcuts: function () {
+    this.inputForm.onsubmit = function (e) {
+      e.preventDefault();
+      this.connection.send(JSON.stringify({
+        type: 'message',
+        room: document.location.hash,
+        text: this.input.value
+      }));
+      this.input.placeholder = 'Chat...';
+      this.inputForm.reset();
+    }.bind(this);
+
+    this.roomSwitcherForm.onsubmit = function (e) {
+      e.preventDefault();
+      // Change room -- part and join (no multiroom support in front end)
+      this.connection.send(JSON.stringify({
+        type: 'part',
+        room: window.location.hash
+      }));
+
+      window.location.hash = this.roomSwitcher.value;
+      this.connection.send(JSON.stringify({
+        type: 'join',
+        room: window.location.hash
+      }));
+
+      this.roomSwitcher.value = window.location.hash;
+      this.roomSwitcher.placeholder = window.location.hash;
+
+      // Grab history of new room
+      this.clearContent();
+      this.connection.send(JSON.stringify({
+        type: 'history',
+        room: window.location.hash
+      }));
+    }.bind(this);
+  },
+
+  clearContent: function () {
+    this.content.innerHTML = '';
+    this.currentBoxActors = 0;
+    this.currentBoxes = 0;
+  },
+
+  addHistory: function (history) {
+    for (var i = 0; i < history.length; i++) {
+      this.addLine(JSON.parse(history[i]), false);
+    }
+    window.scrollTo(0, document.body.scrollHeight);
+  },
+
+  addLine: function (message, stickBottom) {
+    // Make a new box if
+    // * We hit maximum number of actors in a box
+    // * No boxes
+    // * It's a monologue
+    var newBox =
+      this.currentBoxActors >= this.maxActorsPerBox ||
+      this.currentBoxes === 0 ||
+      this.previousAuthor === message.author;
+
+    if (newBox === true) {
+      this.currentBox = this.makeBox();
+      this.content.appendChild(this.currentBox);
+      if (typeof stickBottom === 'undefined' || stickBottom === true) {
+        window.scrollTo(0, document.body.scrollHeight);
+      }
+      this.currentBoxActors = 0;
+      this.currentBoxes++;
+    }
+
+    var flip = this.currentBoxActors >= this.maxActorsPerBox / 2;
+
+    this.currentBox.appendChild(this.makeActor(message, flip));
+    this.currentBoxActors++;
+    this.previousAuthor = message.author;
+  },
+
+  makeBox: function () {
+    var boxTemplate = document.getElementById('box-template').innerHTML;
+    var box = document.createElement('div');
+    box.innerHTML = boxTemplate;
+
+    return box.getElementsByTagName('div')[0];
+  },
+
+  makeActor: function (message, flip) {
+    var actorTemplate = document.getElementById('actor-template').innerHTML;
+    var characters = this.characters;
+
+    var character = characters[this.getHashCode(message.author) % characters.length];
+    var avatar = document.createElement('img');
+    var avatarImageIndex = this.getHashCode(message.text + ' ' + message.author + ' ' + this.currentBoxes) % character.images.length;
+    avatar.src = './res/avatars/' + character.name + '/' + character.images[avatarImageIndex] + '.png';
+
+    // Make characters face each other
+    if (flip === true) {
+      if (avatar.classList) {
+        avatar.classList.add('flip-horizontal');
+      } else {
+        avatar.className += ' flip-horizontal';
+      }
+    }
+
+    var actor = document.createElement('div');
+    actor.innerHTML = actorTemplate;
+    actor.querySelector('.text').appendChild(document.createTextNode(message.text));
+    actor.querySelector('.name').appendChild(document.createTextNode(message.author));
+    actor.querySelector('.avatar').appendChild(avatar);
+
+    return actor.getElementsByTagName('div')[0];
+  },
+
+  loadCharacterManifest: function () {
+    var request = new XMLHttpRequest();
+    request.open('GET', './res/avatars/manifest.json', true);
+    request.send();
+
+    var that = this;
+    request.onload = function() {
+      that.characters = JSON.parse(this.response);
+    };
+  },
+
+  getHashCode: function (string) {
+    var hash = 31;
+
+    if (string.length === 0) {
+      return hash;
+    }
+
+    for (var i = 0; i < string.length; i++) {
+      hash = ((hash << 5) - hash) + string.charCodeAt(i);
+      hash = hash & hash; // Convert to 32bit integer
+    }
+
+    return Math.abs(hash);
+  }
+};
