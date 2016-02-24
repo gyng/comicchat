80e079dc5ad302251f8dcbdaf1c54d34a58b3592
Ng Guoyou <fauxface+github@gmail.com>
2016-02-25 00:39:09 +0800

    Updates

--- js/client.js
+++ js/client.js
@@ -1,7 +1,7 @@
 (function () {
   'use strict';
 
-  var serverAddress = 'ws://localhost:8084';
+  var serverAddress = 'ws://home.sugoi.pw:8084';
 
   var ui = new UI({
     content:          document.getElementById('content'),
