808fe8245c04010e824ab4aa79ce96c903f4682c
Ng Guoyou <ng.guoyou@gmail.com>
2016-03-23 18:36:15 +0800

    Wire up the threads for the ws server

--- server/rust/src/main.rs
+++ server/rust/src/main.rs
@@ -2,7 +2,8 @@ extern crate websocket;
 extern crate rustc_serialize;
 
 use std::thread;
-use std::sync::Arc;
+use std::sync::{Arc, Mutex};
+use std::sync::mpsc::channel;
 use rustc_serialize::json;
 use websocket::{Server, Message, Sender, Receiver};
 use websocket::message::Type;
@@ -14,23 +15,41 @@ use websocket::WebSocketStream;
 #[derive(RustcDecodable, RustcEncodable, Debug)]
 pub struct ComicMessage {
     message_type: String,
-    room: String,
-    time: String,
-    text: String,
-    author: String
+    text: String
+    // room: String,
+    // time: String,
+    // text: String,
+    // author: String
 }
 
 fn main() {
-    // TODO: Read config
     let host = "0.0.0.0:8084";
     let server = Server::bind(host).unwrap();
-    // let clients = Arc::new(Vec::new());
+
+    let (broadcast_tx, broadcast_rx) = channel::<Message>();
+    let clients: Arc<Mutex<Vec<std::sync::mpsc::Sender<Message>>>> = Arc::new(Mutex::new(Vec::new()));
+
+    // Cross-client broadcast
+    let broadcast_connections = clients.clone();
+    thread::spawn(move || {
+        println!("broadcast thread running");
+        for message in broadcast_rx.iter() {
+            for conn_tx in broadcast_connections.lock().unwrap().iter() {
+                // println!("Sending to client: {}", message.clone());
+                conn_tx.send(message.clone());
+            }
+        }
+    });
 
     for connection in server {
+        let broadcast_tx_local = broadcast_tx.clone();
+        let (conn_tx, conn_rx) = channel::<Message>();
+        clients.lock().unwrap().push(conn_tx.clone());
+
+        // Receive from client
         thread::spawn(move || {
             let request = connection.unwrap().read_request().unwrap();
             let headers = request.headers.clone();
-
             request.validate().unwrap();
 
             let mut response = request.accept();
@@ -42,9 +61,7 @@ fn main() {
             }
 
             let mut client = response.send().unwrap();
-            // clients.push(&client);
             let client_ip = client.get_mut_sender().get_mut().peer_addr().unwrap();
-
             println!("Connection from {}", client_ip);
 
             let message: Message = Message::text("Connected".to_string());
@@ -52,43 +69,42 @@ fn main() {
 
             let (mut sender, mut receiver) = client.split();
 
-            for message in receiver.incoming_messages() {
-                let message: Message = message.unwrap();
+            thread::spawn(move || {
+                for message in receiver.incoming_messages() {
+                    let message: Message = message.unwrap();
 
-                match message.opcode {
-                    Type::Close => {
-                        let message = Message::close();
-                        sender.send_message(&message).unwrap();
-                        println!("Client {} disconencted", client_ip);
-                        return;
-                    },
-                    Type::Ping => {
-                        let message = Message::pong(message.payload);
-                        sender.send_message(&message).unwrap();
-                    },
-                    Type::Text => {
-                        println!("Text");
-                        handle(&mut sender, message);
-                    },
-                    _ => sender.send_message(&message).unwrap()
+                    match message.opcode {
+                        Type::Close => {
+                            println!("Client {} disconencted", client_ip);
+                            conn_tx.send(Message::close());
+                            return;
+                        },
+                        Type::Ping => {
+                            conn_tx.send(Message::pong(message.payload));
+                        },
+                        Type::Text => {
+                            let payload = String::from_utf8(message.payload().into_owned()).expect("failed to convert payload into string");
+                            let deserialized: ComicMessage = json::decode(&payload).expect("failed to decode JSON");
+                            println!("Received: {:?}", deserialized);
+
+                            match deserialized.message_type.as_ref() {
+                                "message" => broadcast_tx_local.send(Message::text(deserialized.text)).unwrap(),
+                                _ => return // closes connection
+                            }
+                        },
+                        _ => return
+                    }
                 }
-            }
+            });
+
+            // Respond to client
+            thread::spawn(move || {
+                for message in conn_rx.iter() {
+                    println!("yay!");
+                    sender.send_message(&message);
+                }
+            });
         });
-    }
-}
-
-fn handle<T: Sender>(sender: &mut T, message: Message) -> Result<(), ()> {
-    let payload = String::from_utf8(message.payload().into_owned()).expect("failed to convert payload into string");
-    let decoded: ComicMessage = json::decode(&payload).expect("failed to decode JSON");
-    println!("{:?}", decoded);
-
-    match decoded.message_type.as_ref() {
-        "message" => {
-            sender.send_message(&message).expect("failed to send message in text");
-            // How to broadcast?
-            Ok(())
-        },
-        _ => Ok(()) // unknown
     }
 }
 
